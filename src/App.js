c6297acaa004de10d6741555e4015945c3e2502e
nitinsolanki291299 <nitin.s@dharveeinfotech.com>
2023-09-14 04:00:48 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Skill from './components/Skill';
-import Educaation from './components/Educaation';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Skill from "./components/Skill";
+import Educaation from "./components/Educaation";
+import Contact from "./components/Contact";
+
+import Footer from "./components/Footer";
 
 export default class App extends Component {
   render() {
